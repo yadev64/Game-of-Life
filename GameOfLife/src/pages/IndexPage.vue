ce8c1037c0cfbdba4affa97d0826c3cd01ba8886
Yadev Jayachandran <yadev64@gmail.com>
2023-03-29 02:11:16 +0530

    Added Options for grids

--- GameOfLife/src/pages/IndexPage.vue
+++ GameOfLife/src/pages/IndexPage.vue
@@ -10,8 +10,12 @@
       </q-card>
       <q-select
         filled
+        transition-show="flip-up"
+        transition-hide="flip-down"
         class="q-my-lg shadow-10 bg-orange-3"
         style="width: 100%; border-radius: 10px"
+        v-model="selectedGrid"
+        :options="gridOptions"
       ></q-select>
       <div class="row flex flex-center">
         <q-btn
@@ -25,9 +29,54 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { ref, defineComponent } from "vue";
 
 export default defineComponent({
   name: "IndexPage",
+  setup() {
+    let gridOptions = ref([
+      {
+        id: 1,
+        label: "10 x 10",
+        row: 10,
+        col: 10,
+      },
+      {
+        id: 2,
+        label: "20 x 20",
+        row: 20,
+        col: 20,
+      },
+      {
+        id: 3,
+        label: "30 x 30",
+        row: 30,
+        col: 30,
+      },
+      {
+        id: 4,
+        label: "50 x 50",
+        row: 50,
+        col: 50,
+      },
+      {
+        id: 5,
+        label: "100 x 100",
+        row: 100,
+        col: 100,
+      },
+    ]);
+
+    let selectedGrid = ref({
+      id: 1,
+      label: "10 x 10",
+      row: 10,
+      col: 10,
+    });
+    return {
+      gridOptions,
+      selectedGrid,
+    };
+  },
 });
 </script>
