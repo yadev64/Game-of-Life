56c06015d8bec4c134c4883738a2e0e82c5e4f02
Yadev Jayachandran <yadev64@gmail.com>
2023-04-01 10:55:44 +0530

    Grid Component initiated. - Uses CellComponent to render cells

---
+++ GameOfLife/src/components/GridComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-card class="bg-white q-pa-md q-ma-md" style="width: 100%; height: 100%">
+    {{ grid[0][5] }}
+    {{ gameStore.grid }}
+
+    <cellComponent :isAlive="true"></cellComponent>
+    <cellComponent :isAlive="false"></cellComponent>
+  </q-card>
+</template>
+
+<script>
+import { useRouter } from "vue-router";
+import { ref, defineComponent, computed } from "vue";
+import { useCounterStore } from "../stores/GameConfig";
+import cellComponent from "../components/CellComponent.vue";
+
+export default {
+  components: {
+    cellComponent,
+  },
+  setup() {
+    const gameStore = useCounterStore();
+    const router = useRouter();
+
+    const grid = gameStore.grid;
+
+    const mutateGrid = () => {
+      grid[7][1] = 5;
+    };
+
+    return {
+      grid,
+      mutateGrid,
+      gameStore,
+    };
+  },
+  name: "GridComponent",
+};
+</script>
