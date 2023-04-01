99288127295686d4a645f8a541c90a9328a0a933
Yadev Jayachandran <yadev64@gmail.com>
2023-04-01 10:55:21 +0530

    Cell Component created - Changes color based on isAlive state

---
+++ GameOfLife/src/components/CellComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-card :class="[isAlive ? 'bg-red' : 'bg-white', 'cell']">{{}}</q-card>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "CellComponent",
+  props: {
+    isAlive: {
+      default: false,
+      type: Boolean,
+    },
+  },
+  setup(props) {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.cell {
+  height: 50px;
+  width: 50px;
+  border: 1px solid black;
+  border-radius: 0px;
+}
+</style>
