584943d57ee2a29d173dd420393fb43788bb5292
Yadev Jayachandran <yadev64@gmail.com>
2023-04-01 10:56:00 +0530

    Game Page created - Uses Grid Component to show the grid

---
+++ GameOfLife/src/pages/GamePage.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-page class="bg-green-4" style="height: 100vh; width: 100vw">
+    <div class="row">
+      <div class="col-md-9 col-sm-12 col-xs-12">
+        <GridComponent></GridComponent>
+        <q-btn label="mutate" @click="mutateGrid"></q-btn>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { useRouter } from "vue-router";
+import { ref, defineComponent, computed } from "vue";
+import { useCounterStore } from "../stores/GameConfig";
+import GridComponent from "../components/GridComponent.vue";
+
+export default {
+  components: {
+    GridComponent,
+  },
+
+  setup() {
+    const gameStore = useCounterStore();
+    const router = useRouter();
+
+    const grid = gameStore.grid;
+
+    const mutateGrid = () => {
+      grid[7][1] = 5;
+    };
+
+    return {
+      grid,
+      mutateGrid,
+      gameStore,
+    };
+  },
+  // name: 'PageName',
+};
+</script>
