5321f7a5288afdfe202c405fe9dc69a5aa4db2b6
Yadev Jayachandran <yadev64@gmail.com>
2023-05-05 21:22:10 +0530

    Mutation logic fixes and updates

--- GameOfLife/src/pages/GamePage.vue
+++ GameOfLife/src/pages/GamePage.vue
@@ -28,38 +28,54 @@ export default {
     let grid2 = gameStore.grid2;
 
     const mutateGrid = () => {
-      grid2 = JSON.parse(JSON.stringify(grid));
+      // console.log("Grid2init:", grid2);
+      // grid2 = JSON.parse(JSON.stringify(grid));
 
-      let liveCount = 1;
+      copyGrid(0);
 
-      while (liveCount > 0) {
-        setTimeout(() => {
-          for (let i = 0; i < grid[0].length; i++) {
-            for (let j = 0; j < grid.length; j++) {
-              if (liveNeighbourCount(i, j) == 3 && grid[i][j] == 0) {
-                // bring back to life
-                grid2[i][j] = 5;
-              } else if (liveNeighbourCount(i, j) in [2, 3]) {
-                // Keep cell
-                grid2[i][j] = grid[i][j];
-              } else if (liveNeighbourCount(i, j) > 3) {
-                // Kill cell
-                grid2[i][j] = 0;
-              } else if (liveNeighbourCount(i, j) < 2) {
-                // Kill cell
-                grid2[i][j] = 0;
-              }
+      console.log("Grid2init:", JSON.parse(JSON.stringify(grid2)));
+
+      // let liveCount = 1;
+
+      // while (liveCount > 0) {
+      setTimeout(() => {
+        for (let i = 0; i < grid[0].length; i++) {
+          for (let j = 0; j < grid.length; j++) {
+            if (liveNeighbourCount(i, j) == 3 && grid[i][j] == 0) {
+              console.log("MUTATED,1");
+              // bring back to life
+              grid2[i][j] = 5;
+            } else if (
+              liveNeighbourCount(i, j) == 2 ||
+              liveNeighbourCount(i, j) == 3
+            ) {
+              console.log("MUTATED,2", liveNeighbourCount(i, j));
+              // Keep cell
+              grid2[i][j] = grid[i][j];
+            } else if (liveNeighbourCount(i, j) > 3) {
+              console.log("MUTATED,3");
+              // Kill cell
+              grid2[i][j] = 0;
+            } else if (liveNeighbourCount(i, j) < 2) {
+              console.log("MUTATED,4");
+              // Kill cell
+              grid2[i][j] = 0;
             }
           }
+        }
 
-          grid = JSON.parse(JSON.stringify(grid2));
-        }, 1000);
-      }
+        console.log("Grid2exit:", grid2);
+
+        // grid = JSON.parse(JSON.stringify(grid2));
+
+        copyGrid(1);
+      }, 500);
+      // }
       // grid[7][1] = 5;
     };
 
     const liveNeighbourCount = (i, j) => {
-      neighbourCount = 0;
+      let neighbourCount = 0;
 
       let is = i - 1 >= 0 ? i - 1 : i;
       let ie = i + 1 < grid.length ? i + 1 : i;
@@ -74,7 +90,22 @@ export default {
         }
       }
 
+      // console.log("NBC: ", neighbourCount, i, j);
       return neighbourCount;
+    };
+
+    const copyGrid = (mode) => {
+      for (let i = 0; i < grid[0].length; i++) {
+        for (let j = 0; j < grid.length; j++) {
+          if (mode == 1) {
+            // grid = grid2
+            grid[i][j] = grid2[i][j];
+          } else {
+            // grid2 = grid
+            grid2[i][j] = grid[i][j];
+          }
+        }
+      }
     };
 
     return {
